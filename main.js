b03bd19da969f85494e72e2bdbec633903ec55ff
SanchitaSubramaniam <86506500+SanchitaSubramaniam@users.noreply.github.com>
2021-11-10 18:24:13 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 var first_name = "Alyssa";
+console.log(first_name);
 var last_name = "Carson";
+console.log(last_name);
 var full_name = first_name.concat(last_name);
 console.log(full_name);
 var a = 20;
+console.log(a);
 var b = 9;
+console.log(b);
 var add = a + b;
-console.log(add);  
+console.log(add);
